9aab20092e599b778d3da07cebefc17836a53678
Amr Walidi <amr.nawaf@gmail.com>
2024-11-13 18:01:29 +0300

    add images

--- yolo_roboflow_image_training.py
+++ yolo_roboflow_image_training.py
@@ -5,9 +5,9 @@ model = YOLO('yolov8n.pt')
 
 rf = Roboflow(api_key="zGjCRPBrA5e9YQJSr5vo")
 project = rf.workspace("computer-vision-cuvyr").project("facial-recognition-gcop7")
-version = project.version(1)
+version = project.version(5)
 dataset = version.download("yolov8")
 
 if __name__ == "__main__" :
-    model.train(data=f"{dataset.location}/data.yaml", epochs=100)
+    model.train(data=f"{dataset.location}/data.yaml", epochs=50)
 
